dde5958e277160d8f25dae19144d778f411a9e39
Zickrom14 <lumogdanglouise@isufst.edu.ph>
2024-11-06 11:28:24 +0800

    Delete messenger_project directory

--- messenger_project/chat/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.chat_list, name='chat_list'),
-    path('new/', views.chat_create, name='chat_create'),
-    path('delete/<int:pk>/', views.chat_delete, name='chat_delete'),
-]
